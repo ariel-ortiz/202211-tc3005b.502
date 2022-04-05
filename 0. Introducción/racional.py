5889a71a348cc212bed789610570bda03ed30ba3
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-04-05 23:59:22 +0000

    Agrega métodos y propiedades a la clase Racional

--- 0. Introducción/racional.py
+++ 0. Introducción/racional.py
@@ -14,8 +14,19 @@ class Racional:
         self.__numerador = numerador // comun * signo
         self.__denominador = denominador // comun
 
+    @property
+    def numerador(self):
+        return self.__numerador
+
+    @property
+    def denominador(self):
+        return self.__denominador
+
     def __str__(self):
         return f'{self.__numerador}/{self.__denominador}'
+
+    def __repr__(self):
+        return f'Racional({self.__numerador}, {self.__denominador})'
 
     def __eq__(self, other):
         return (self.__numerador == other.__numerador and
@@ -31,21 +42,38 @@ class Racional:
                 self.__denominador * other.__numerador,
             self.__denominador * other.__denominador)
 
+    def __int__(self):
+        return self.__numerador // self.__denominador
+
+    def __float__(self):
+        return self.__numerador / self.__denominador
+
+    def __hash__(self):
+        return hash(float(self))
+
 # Ejemplos de uso
 
 a = Racional()
-print(f'a:         {a}')
+print(f'a:             {a}')
 b = Racional(1, 2)
-print(f'b:         {b}')
+print(f'b:             {b}')
 c = Racional(1, -3)
-print(f'c:         {c}')
+print(f'c:             {c}')
 d = b + c
-print(f'd = b + c: {d}')
+print(f'd = b + c:     {d}')
 e = Racional(1, 6)
-print(f'e:         {e}')
-print(f'e == d:    {e == d}')
-print(f'd != e:    {d != e}')
-print(f'c < b:     {c < b}')
-print(f'e <= d:    {e <= d}')
-print(f'c > b:     {c > b}')
-print(f'e >= d:    {e >= d}')
+print(f'e:             {e}')
+print(f'e == d:        {e == d}')
+print(f'd != e:        {d != e}')
+print(f'c < b:         {c < b}')
+print(f'e <= d:        {e <= d}')
+print(f'c > b:         {c > b}')
+print(f'e >= d:        {e >= d}')
+print(f'e.numerador:   {e.numerador}')
+print(f'e.denominador: {e.denominador}')
+print(f'hash(d):       {hash(d)}')
+print(f'hash(e):       {hash(e)}')
+print(f'int(b):        {int(b)}')
+print(f'float(b):      {float(b)}')
+s = {a, e, d}
+print(f's:             {s}')
